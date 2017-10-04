a3b8d963f5042f270de70a40c51ce308bbb2f87a
Pavel Koshkin <koshkin.pasha@gmail.com>
2017-10-04 22:21:35 +0300

    Ready.

--- seminars/3/template.py
+++ seminars/3/template.py
@@ -34,18 +34,18 @@ def train_hmm(tagged_sents):
         tags.update({tag for word, tag in sentence})
         words.update({word for word, tag in sentence})
 
-    p_t_t = defaultdict(lambda: defaultdict(float))
-    p_w_t = defaultdict(lambda: defaultdict(float))
-    p_t = defaultdict(float)
+    p_t_t = defaultdict(lambda: defaultdict(lambda: 1e-50))
+    p_w_t = defaultdict(lambda: defaultdict(lambda: 1e-50))
+    p_t = defaultdict(lambda: 1e-50)
     # Computing probabilities
     for tag in tags:
         for word in words:
-            p_w_t[word][tag] += ((counter_tag_word[(tag, word)] + delta) /
-                                 (counter_tag[tag] + delta * len(tags)))
+            p_w_t[tag][word] = ((counter_tag_word[(tag, word)] + delta) /
+                                 (counter_tag[tag] + delta * len(words)))
         for next_tag in tags:
-            p_t_t[tag][next_tag] += ((counter_tag_tag[(tag, next_tag)] + delta) /
+            p_t_t[tag][next_tag] = ((counter_tag_tag[(tag, next_tag)] + delta) /
                                  (counter_tag[tag] + delta * len(tags)))
-        p_t[tag] += 1 / len(tags)
+        p_t[tag] = 1 / len(tags)
 
     return p_t, p_w_t, p_t_t
 
@@ -68,22 +68,22 @@ def viterbi_algorithm(test_tokens_list, p_t, p_w_t, p_t_t):
     
     tags = np.array([key for key in p_t.keys()])
     delta = [np.array([
-        p_w_t[test_tokens_list[0]][k] * p_t[k] for k in tags
+        np.log(p_w_t[k][test_tokens_list[0]]) + np.log(p_t[k]) for k in tags
     ])]
     s = []
     for i in range(1, len(test_tokens_list)):
         delta.append(np.array([
-            np.max([p_w_t[test_tokens_list[i]][k] * p_t_t[m][k] * delta[i - 1][m] for m in range(len(tags))])
+            np.max([np.log(p_w_t[k][test_tokens_list[i]]) + np.log(p_t_t[tags[m]][k]) + delta[i - 1][m] for m in range(len(tags))])
             for k in tags
         ]))
         s.append(np.array([
-            np.argmax([p_w_t[test_tokens_list[i]][k] * p_t_t[m][k] * delta[i - 1][m] for m in range(len(tags))])
+            np.argmax([np.log(p_w_t[k][test_tokens_list[i]]) + np.log(p_t_t[tags[m]][k]) + delta[i - 1][m] for m in range(len(tags))])
             for k in tags
         ]))
     delta = np.array(delta)
     
-    result = [tags[np.argmax(delta[-1, :])]]
     previous_link = np.argmax(delta[-1, :])
+    result = [tags[previous_link]]
     for link in reversed(s):
         result.append(tags[link[previous_link]])
         previous_link = link[previous_link]
